23c1954021fff8031fefa6470d8bf3f062e858f5
matheuspsiqueira <matheuspontessiqueira@gmail.com>
2025-05-29 15:48:41 -0300

    27° atualização

--- static/js/script_sobre.js
+++ static/js/script_sobre.js
@@ -6,3 +6,27 @@ const carousel = document.querySelector('.hero-carousel');
     index = (index + 1) % slides.length;
     carousel.style.transform = `translateX(-${index * 100}%)`;
   }, 5000); // a cada 5 segundos
+
+
+  const carrossel = document.getElementById("carrossel");
+  const leftBtn = document.querySelector(".btn-nav.left");
+  const rightBtn = document.querySelector(".btn-nav.right");
+
+  function scrollLeft() {
+    carrossel.scrollBy({ left: -220, behavior: 'smooth' });
+  }
+
+  function scrollRight() {
+    carrossel.scrollBy({ left: 220, behavior: 'smooth' });
+  }
+
+  leftBtn.addEventListener("click", scrollLeft);
+  rightBtn.addEventListener("click", scrollRight);
+
+  // Auto-scroll infinito
+  setInterval(() => {
+    scrollRight();
+    if (carrossel.scrollLeft + carrossel.offsetWidth >= carrossel.scrollWidth) {
+      carrossel.scrollTo({ left: 0, behavior: 'smooth' });
+    }
+  }, 4000);
